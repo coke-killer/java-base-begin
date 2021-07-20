61817b244213fbee70fdd299ef3e61fc2ee352e0
yudongyue <oakyue@outlook.com>
2021-07-20 19:02:46 +0800

    Initial commit

---
+++ src/main/java/com/example/javabasebegin/JavaBaseBeginApplication.java
@@ -0,0 +1,13 @@
+package com.example.javabasebegin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBaseBeginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBaseBeginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javabasebegin/JavaBaseBeginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javabasebegin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBaseBeginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
